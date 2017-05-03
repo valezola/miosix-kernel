80843ca984b9280d1269e94ed146664ab730a996
Valeria Mazzola <circadianarchist@gmail.com>
2017-05-03 17:37:31 +0200

    Initialize a few CTPM registers

--- miosix/arch/cortexM4_stm32f4/stm32f469ni_stm32f469i-disco/drivers/lcd_touchscreen.cpp
+++ miosix/arch/cortexM4_stm32f4/stm32f469ni_stm32f469i-disco/drivers/lcd_touchscreen.cpp
@@ -1,7 +1,9 @@
-#ifdef LCD_TOUCHSCREEN
+#ifdef LCD_TOUCHSCREEN 
 
 #include "drivers/stm32f2_f4_i2c.h"
 #include "lcd_touchscreen.h"
+
+#define LCD_I2C_ADDR ((unsigned char) 0x54) 
 
 using namespace miosix;
 
@@ -14,20 +16,38 @@ TouchscreenDriver& TouchscreenDriver::instance()
 
 void TouchscreenDriver::init()
 {
+    static I2C1Driver& i2c = I2C1Driver::instance();
+    i2c.init();
+
+    unsigned char device_mode[2];
+    device_mode[0] = 0x00; // Address of the Device Mode register in the CTPM
+    device_mode[1] = 0x00; // Set the CTPM in Working Mode
+
+    /*
+     * Set all writable registers to their default values
+     */
+    unsigned char ctrl[2];
+    ctrl[0] = 0x86; // Address of CTRL register in the CTPM
+    ctrl[1] = 0x01; // Set the CTPM to switch to monitor mode with no touching
+
+    /*
+     * Set the interrupt working mode of the device to Trigger mode
+     * */
+    unsigned char g_mode[2];
+    g_mode[0] = 0xA4; // Address of the G_MODE register in the CTPM
+    g_mode[1] = 0x01; // Set the CTPM in Interrupt Trigger mode, as default
+
+    /* 
+     * Send all initialization values to the CTPM over I2C
+     */
+    i2c.send(LCD_I2C_ADDR, device_mode, 2);
+    i2c.send(LCD_I2C_ADDR, ctrl, 2);
+    i2c.send(LCD_I2C_ADDR, g_mode, 2);
+
 }
 
 void TouchscreenDriver::read_gesture(struct gesture_data_t& gesture_data)
 {
-    //wait for interrupt
-    //read from i2c registers 
-    //check that slave address is correct: 0x54
-    //data packet:
-    //
-    //A new interrupt is issued by the TouchScreen controller every time a packet is sent
-    //This means that, upon receiving an interrupt, only one packet needs to be read
-    //(interrupt polling mode)
-    //Actually, packet bursts are also possible (interrupt trigger mode)
-    //
 }
 
 #endif //LCD_TOUCHSCREEN
