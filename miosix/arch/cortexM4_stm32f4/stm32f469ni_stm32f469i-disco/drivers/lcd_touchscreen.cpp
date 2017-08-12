1a186d8fbfa8ee18b3520209323a51ffb5f7c987
Valeria Mazzola <circadianarchist@gmail.com>
2017-08-12 15:54:44 +0200

    Define interrupt handler
            - update init function to initialize NVIC interrupt
            - read_gesture no longer reads registers, but returns formatted
              data from private buffer

--- miosix/arch/cortexM4_stm32f4/stm32f469ni_stm32f469i-disco/drivers/lcd_touchscreen.cpp
+++ miosix/arch/cortexM4_stm32f4/stm32f469ni_stm32f469i-disco/drivers/lcd_touchscreen.cpp
@@ -4,11 +4,14 @@
 #include "lcd_touchscreen.h"
 
 #define LCD_I2C_ADDR ((unsigned char) 0x54) 
+#define BUFLEN (0xbd)
 
 using namespace miosix;
 
 
 static I2C1Driver& i2c = I2C1Driver::instance();
+
+unsigned char data_array[BUFLEN]; // received data buffer
 
 TouchscreenDriver& TouchscreenDriver::instance()
 {
@@ -17,35 +20,80 @@ TouchscreenDriver& TouchscreenDriver::instance()
     
 }
 
-void TouchscreenDriver::init()
-{
+
+void EXTI9_5_IRQHandler() {
+    saveContext();
+    //read_reg(0x00, data_array, BUFLEN);
+    restoreContext();
+}
+
+void TouchscreenDriver::init() {
+
     i2c.init();
+
+    /* Clear packet buffer first */
+    int i;
+    for (i=0; i<BUFLEN; i++) {
+        data_array[i] = 0x00;
+    }
+
+    /*
+     * Inizialize NVIC interrupt line corresponding to EXTI5
+     */
+    NVIC_SetPriority(EXTI9_5_IRQn,10); //Same priority as i2c bus
+    NVIC_ClearPendingIRQ(EXTI9_5_IRQn);
+    NVIC_EnableIRQ(EXTI9_5_IRQn);
+
 
     /*
      * Set all writable registers to their default values
      */
 
     uint8_t device_mode = 0x00; // Set the CTPM to Working Mode
+    //uint8_t thresh
+    //filter <--- default value for these not specified on datasheet
     uint8_t ctrl = 0x01; // Set the CTPM to switch to monitor mode with no touching
+    uint8_t time_enter_monitor = 0x0A; // Timeout to enter monitor mode when no touch
+    // report rate in active mode
+    uint8_t period_monitor = 0x28; // Report rate in monitor mode
+    uint8_t radian = 0x0A; // Minimum angle for rotation
+    uint8_t offset_lr = 0x19; // Max offset for left and right gesture
+    uint8_t offset_ud = 0x19; // Max offset for up and down gesture
+    uint8_t distance_rl = 0x19; // Max distance for left and right gesture
+    uint8_t distance_ud = 0x19; // Max distance for up and down gesture
+    uint8_t distance_zoom = 0x32; // Max distance for up and down gesture
+
     uint8_t g_mode = 0x01; // Set the CTPM to Interrupt Trigger mode, as default
+    uint8_t power_mode = 0x00; 
+    uint8_t state = 0x01; // Operating mode
 
     /* 
      * Send all initialization values to the CTPM over I2C
      */
     write_reg(0x00, &device_mode, 1);
     write_reg(0x86, &ctrl, 1);
-    write_reg(0xA4, &g_mode, 1);
+    write_reg(0x87, &time_enter_monitor, 1);
+    write_reg(0x89, &period_monitor, 1);
+    write_reg(0x91, &radian, 1);
+    write_reg(0x92, &offset_lr, 1);
+    write_reg(0x93, &offset_ud, 1);
+    write_reg(0x94, &distance_rl, 1);
+    write_reg(0x95, &distance_ud, 1);
+    write_reg(0x96, &distance_zoom, 1);
 
+    write_reg(0xA4, &g_mode, 1);
+    write_reg(0xA5, &power_mode, 1);
+    write_reg(0xBC, &state, 1);
 
 }
 
 void TouchscreenDriver::read_gesture(struct gesture_data_t& gesture_data)
 {
+
     /*
-     * Read all the touch data as a byte array
+     * We use the buffered data from the last read packet to fill the
+     * gesture_data structure
      */
-    uint8_t data_array[15];
-    read_reg(0x00, data_array, 15);
 
     /*
      * Fill gesture ID and number of touches
@@ -124,8 +172,11 @@ void TouchscreenDriver::read_reg(unsigned char reg_addr, unsigned char *data, in
      * the address of the first register to be read. It appears to be possible to
      * read multiple registers in a single read command.
      */
+
     i2c.send(LCD_I2C_ADDR, &reg_addr, 1);
     i2c.recv(LCD_I2C_ADDR, data, len);
 }
 
+#undef LCD_I2C_ADDR 
+#undef BUFLEN 
 #endif //LCD_TOUCHSCREEN
--- miosix/arch/cortexM4_stm32f4/stm32f469ni_stm32f469i-disco/drivers/lcd_touchscreen.h
+++ miosix/arch/cortexM4_stm32f4/stm32f469ni_stm32f469i-disco/drivers/lcd_touchscreen.h
@@ -6,48 +6,51 @@
 namespace miosix
 {
 
-enum gesture_t {
-    MOVE_UP, MOVE_RIGHT, MOVE_DOWN, MOVE_LEFT,
-    ZOOM_IN, ZOOM_OUT, NO_GESTURE
-};
+    enum gesture_t {
+        MOVE_UP, MOVE_RIGHT, MOVE_DOWN, MOVE_LEFT,
+        ZOOM_IN, ZOOM_OUT, NO_GESTURE
+    };
 
-enum event_t {
-    PRESS_DOWN,
-    LIFT_UP,
-    CONTACT,
-    NO_EVENT
-};
+    enum event_t {
+        PRESS_DOWN,
+        LIFT_UP,
+        CONTACT,
+        NO_EVENT
+    };
 
-struct touch_data_t {
-    enum event_t event;
-    uint8_t touchID;
-    uint8_t area; 
-    uint8_t weight;
-    uint16_t x; 
-    uint16_t y;
+    struct touch_data_t {
+        enum event_t event;
+        uint8_t touchID;
+        uint8_t area; 
+        uint8_t weight;
+        uint16_t x; 
+        uint16_t y;
 
-};
+    };
 
-struct gesture_data_t {
-    enum gesture_t gestureID;
-    uint8_t num_touches;
-    struct touch_data_t touches[2];
-};
+    struct gesture_data_t {
+        enum gesture_t gestureID;
+        uint8_t num_touches;
+        struct touch_data_t touches[2];
+    };
 
-class TouchscreenDriver {
-public:
-    static TouchscreenDriver& instance();
-    void init();
+    class TouchscreenDriver {
+    public:
 
-    void read_gesture(struct gesture_data_t& gesture_data);
+        static TouchscreenDriver& instance();
+        void init();
 
-private:
-//    TouchscreenDriver(const TouchscreenDriver&);
-//    TouchscreenDriver& operator=(const TouchscreenDriver&);
+        void read_gesture(struct gesture_data_t& gesture_data);
 
-    TouchscreenDriver() { init(); }
-    void write_reg(unsigned char reg_addr, unsigned char *data, int len);
-    void read_reg(unsigned char reg_addr, unsigned char *data, int len);
+    private:
+    //    TouchscreenDriver(const TouchscreenDriver&);
+    //    TouchscreenDriver& operator=(const TouchscreenDriver&);
+
+        //TouchscreenDriver() {  }
+        TouchscreenDriver() { init(); }
+        void write_reg(unsigned char reg_addr, unsigned char *data, int len);
+        void read_reg(unsigned char reg_addr, unsigned char *data, int len);
+        
 };
 
 } //namespace miosix
